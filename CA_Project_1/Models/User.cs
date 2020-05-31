c53ca125365d118b7c00a7c37f2f4c4adba19f70
jadeaclim <60651160+jadeaclim@users.noreply.github.com>
2020-06-01 01:38:17 +0800

    Add files via upload

---
+++ CA_Project_1/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using APIGateway.DB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGateway.Models
+{
+    public class User
+    {
+        [NotMapped]
+        public string ErrMsg { set; get; }
+
+        [MaxLength(36)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Id { get; set; }
+
+        [Required(AllowEmptyStrings=false, ErrorMessage="This field is required.")]
+        public string Username { get; set; }
+
+        [Required(AllowEmptyStrings=false, ErrorMessage="This field is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
