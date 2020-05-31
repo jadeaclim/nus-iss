99a1eb01ed7b18fe7bc2747aedc673427f03606a
jadeaclim <60651160+jadeaclim@users.noreply.github.com>
2020-06-01 01:36:37 +0800

    Add files via upload

---
+++ CA_Project_1/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,18 @@
+﻿using APIGateway.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGateway.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareExtensions(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<Middleware>();
+            return app;
+        }
+    }
+}
