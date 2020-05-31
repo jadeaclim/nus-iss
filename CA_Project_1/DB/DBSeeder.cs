2a3bb9cdae6166fe29b3b4951da586c84e179b7e
jadeaclim <60651160+jadeaclim@users.noreply.github.com>
2020-06-01 01:24:40 +0800

    Add files via upload

---
+++ CA_Project_1/DB/DBSeeder.cs
@@ -0,0 +1,34 @@
+﻿using APIGateway.Models;
+using APIGateway.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGateway.DB
+{
+    public class DBSeeder
+    {
+        public DBSeeder(UserContext dbcontext)
+        {
+
+            User user1 = new User();
+            user1.Id = Guid.NewGuid().ToString();
+            user1.Username = "admin";
+            user1.Password = Crypto.Sha256("admin");
+            user1.Email = "admin@sa50.edu";
+            dbcontext.Add(user1);
+
+            User user2 = new User();
+            user2.Id = Guid.NewGuid().ToString();
+            user2.Username = "john";
+            user2.Password = Crypto.Sha256("john");
+            user2.Email = "john@sa50.edu";
+            dbcontext.Add(user2);
+
+
+            dbcontext.SaveChanges();
+        
+        }
+      }
+   }
---
+++ CA_Project_1/DB/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using APIGateway.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIGateway.DB
+{
+    public class UserContext : DbContext
+    {
+        protected IConfiguration configuration;
+
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+
+        }
+
+        public DbSet<Models.User> User { set; get; }
+
+    }
+}
